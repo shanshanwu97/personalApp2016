aaf1c3b6a91c233b9306e1ad0a99a79ee33106b3
Shanshan Wu <shanshanwu@brandeis.edu>
2016-06-21 10:28:59 -0400

    color counter

--- pages/home/client/home.js
+++ pages/home/client/home.js
@@ -21,3 +21,21 @@ Template.home.events({
 		
 	}
 })
+Template.home.onCreated(function(){
+	this.state= new ReactiveDict();
+	this.state.setDefault({
+		color:"bg-info",
+		counter: 0,
+
+	});
+});
+Template.home.helpers({
+	theColor: function(){
+		const instance = Template.instance();
+		return instance.state.get("color");
+	},
+	theCounter: function(){
+		const instance= Template.instance();
+		return instance.state.get("counter");
+	}
+})
